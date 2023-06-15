573dfbc9ec87d047aa2808f5cb11fdaa78492757
shishupaltech <veerutech75@gmail.com>
2023-06-15 16:40:35 +0530

    database having to error during the connectivity

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 // config 
-dotenv.config({path:"../backend/config/config.env"});
+const port = dotenv.config({path:"backend/config/config.env"});
+
+// connecting to database 
+connectDatabase()
 
 
 app.listen(process.env.PORT,()=>{
