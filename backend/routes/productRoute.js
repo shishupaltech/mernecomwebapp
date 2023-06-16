a85cacf98838b68bf1abc5f05b9402b8fbbbd0bb
shishupaltech <veerutech75@gmail.com>
2023-06-16 11:57:14 +0530

    get details route is define and working fine

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAllProducts,createProduct, updateProduct, deleteProduct } = require("../controllers/productcontroller");
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productcontroller");
 
 const router = express.Router();
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 // url same hai to multiple request you can add with same url
-router.route("/product/:id").put(updateProduct).delete(deleteProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 module.exports = router;
