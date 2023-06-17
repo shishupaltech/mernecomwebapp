e2f4ce1927de04ae8ebbed2b96f20aa33cfb8e51
shishupaltech <veerutech75@gmail.com>
2023-06-17 10:06:56 +0530

    handle the uncaught error when you access the variable but it is not defined in server file then it is called the uncaught Exception if it is occurs then server goes to shutdown.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
+// Handling Uncaught Exception 
+process.on("uncaughtException",(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Error`);
+    process.exit(1);
+})
 
 // config 
 const port = dotenv.config({path:"backend/config/config.env"});
@@ -12,6 +18,7 @@ connectDatabase();
 const server = app.listen(process.env.PORT,()=>{
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 })
+
 
 // unhandled Promise Rejection when server crash means server url going to wrong.
 process.on("unhandledRejection",err=>{
