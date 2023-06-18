65a0045a8a4e1c8f7f31c8fe88fcc9df4780bcc2
shishupaltech <veerutech75@gmail.com>
2023-06-18 11:45:08 +0530

    password is hassed by bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose  = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -40,4 +41,12 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire:Date,
 
 });
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+    
+    this.password = await bcrypt.hash(this.password,10);
+})
 module.exports = mongoose.model("User",userSchema);
