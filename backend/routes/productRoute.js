0818ea7219e2d3e34b19b69cc3b389a28490cf42
shishupaltech <veerutech75@gmail.com>
2023-06-18 14:07:47 +0530

    auth.js add in middleware to check the user varify to access the resources and include in app.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productcontroller");
+const isAuthenticationUser = require("../middleware/auth");
 
 const router = express.Router();
-router.route("/products").get(getAllProducts);
+router.route("/products").get(isAuthenticationUser ,getAllProducts);
 router.route("/product/new").post(createProduct);
 // url same hai to multiple request you can add with same url
 router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
