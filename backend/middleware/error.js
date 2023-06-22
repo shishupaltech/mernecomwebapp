19c99539f3a762b31311c974b089a31e4b376cfb
shishupaltech <veerutech75@gmail.com>
2023-06-22 10:19:19 +0530

    authentication is done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,22 @@ module.exports = (err,req,res,next)=>{
     }
 
 
+    // mongoose duplicate key error 
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message,400)
+    }
+
+    // Wrong JWT error 
+    if(err.code === "jsonWebTokenError"){
+        const message = `Json Web Token is invalid, try again`;
+        err = new ErrorHandler(message,400)
+    }
+    // Wrong JWT Expire Error
+    if(err.code === "TokenExpiredError"){
+        const message = `Json Web Token is Expired, try again`;
+        err = new ErrorHandler(message,400)
+    }
     res.status(err.statusCode).json({
         success:false,
         // error:err you can show the error or you can see the whole stack of error by using this line 
